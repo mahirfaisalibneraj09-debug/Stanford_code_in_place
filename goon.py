51587deb169988037dc38cc6493e52f8b8878fd3
mahirfaisalibneraj09-debug <mahirfaisalibneraj09@gmail.com>
2026-05-06 17:45:29 +0600

    Create a multiplication program in goon.py
    Add a program to multiply two numbers based on user input.

---
+++ goon.py
@@ -0,0 +1,18 @@
+"""
+Program: multiply two numbers
+--------------------
+This program asks the user for two
+integers and prints the value of the first number
+multiplied with the second
+"""
+
+def main():
+    print("This program multiplies two numbers.")
+    num01=int(input("Enter first number: "))
+    num02=int(input("Enter second number: "))
+    num=num01*num02
+    print(num)
+    # your code here
+
+if __name__ == '__main__':
+    main()

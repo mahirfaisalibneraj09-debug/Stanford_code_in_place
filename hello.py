d2bb4d786e60ba403fd337cc8a6d1db27ea658fa
mahirfaisalibneraj09-debug <mahirfaisalibneraj09@gmail.com>
2026-05-06 17:42:19 +0600

    Add main function to greet user by name

---
+++ hello.py
@@ -0,0 +1,11 @@
+def main():
+    x=str(input("What is your name? "))
+    print(f"Hello {x}")
+    """
+    You should write your code here. Make sure to delete 
+    the 'pass' line before starting to write your own code.
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
